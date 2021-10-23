af6f74219dc0a4b30c2f327dec5348cc525c583e
Will 保哥 <doggy.huang@gmail.com>
2021-10-23 16:49:47 +0800

    示範 ngOnInit 與 ngOnChanges 用法

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,17 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent  {
+export class ArticleComponent implements OnInit, OnChanges {
 
   @Input() item: any;
 
   @Output() delete = new EventEmitter<number>();
 
   constructor() { }
+
+  ngOnInit(): void {
+    this.item['abc'] = 1;
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    let a = changes['item'].previousValue;
+    let b = changes['item'].currentValue;
+  }
 
   doDelete() {
     this.delete.emit(this.item.id);
